94d0afe247df4239b15922c9e69d95669424e6bc
Chihiro Snider <chihirosnider@gmail.com>
2026-03-15 15:27:45 -0600

    more files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#C4622D',
+        'accent-light': 'rgba(196,98,45,0.08)',
+        'off-white': '#f8f7f5',
+        'light-gray': '#e8e6e2',
+        'mid-gray': '#888888',
+        'text-gray': '#444444',
+      },
+      fontFamily: {
+        serif: ['"Playfair Display"', 'Georgia', 'serif'],
+        sans: ['Lato', 'sans-serif'],
+      },
+      fontSize: {
+        '2xs': '0.625rem',
+      },
+    },
+  },
+  plugins: [],
+}
